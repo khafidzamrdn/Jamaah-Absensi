d6f79ff68e64bf5e926324962fb5e0aa596eebf8
khafidzamrdn <khafidzamrdn@gmail.com>
2026-09-13 08:57:33 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/absensi/jamaah/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.absensi.jamaah
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    private val viewModel: AppViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // MEMANGGIL TAMPILAN MENU UTAMA YANG BARU
+                    MainAppScreen(viewModel = viewModel)
+                }
+            }
+        }
+    }
+}
